15f9783450790ab06ff32892bd534202122d27f9
Lofendid <102857014+Lofendid@users.noreply.github.com>
2023-12-17 03:13:03 +0100

    ready

---
+++ src/js/02-form.js
@@ -0,0 +1,39 @@
+
+const feedbackForm = document.querySelector('.feedback-form');
+
+const userEmail = feedbackForm.querySelector('input[type="email"]');
+userEmail.value = '';
+
+const userMessage = feedbackForm.querySelector('textarea');
+userMessage.value = '';
+
+const userFeedback = {};
+
+const LS_FEEDBACK = 'feedback-form-state';
+
+if (localStorage.getItem(LS_FEEDBACK)) {
+    const parsedFeedback = JSON.parse(localStorage.getItem(LS_FEEDBACK));
+
+    // ЧИ МОЖНО ТАКИМ:
+    userEmail.value = userFeedback.email = parsedFeedback.email;
+    userMessage.value = userFeedback.message = parsedFeedback.message;
+    // ЧИНОМ ПРИСВОЮВАТИ ОДРАЗУ ДВОМ ЗМІННИМ ЗНАЧЕННЯ, ЧИ НЕ БАЖАНО?
+};
+
+const handleInput = (e) => {
+    if (e.target.name === 'email') userFeedback.email = e.target.value;
+    if (e.target.name === 'message') userFeedback.message = e.target.value;
+    const strUserFeedback = JSON.stringify(userFeedback);
+    localStorage.setItem(LS_FEEDBACK, strUserFeedback);
+};
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(LS_FEEDBACK);
+};
+
+feedbackForm.addEventListener('input', handleInput);
+feedbackForm.addEventListener('submit', handleSubmit);
+
+
